b765fd73697c8fdaabe44aa39e5838f305f4ce4a
MathieuAsusRog <mathieusoufflard@gmail.com>
2020-03-19 14:19:38 +0100

    added all files from old repo (BitBucket) for new one (GitHub)

---
+++ Developpement/DB/Script.sql
@@ -0,0 +1,45 @@
+#------------------------------------------------------------
+#        Script MySQL.
+#------------------------------------------------------------
+
+
+#------------------------------------------------------------
+# Table: Club
+#------------------------------------------------------------
+
+CREATE TABLE Club(
+        Id_Club   Int  Auto_increment  NOT NULL ,
+        Name_Club Varchar (25) NOT NULL
+	,CONSTRAINT Club_PK PRIMARY KEY (Id_Club)
+)ENGINE=InnoDB;
+
+
+#------------------------------------------------------------
+# Table: AccesRight
+#------------------------------------------------------------
+
+CREATE TABLE AccesRight(
+        Id_AccesRight   Int  Auto_increment  NOT NULL ,
+        Name_AccesRight Varchar (50) NOT NULL
+	,CONSTRAINT AccesRight_PK PRIMARY KEY (Id_AccesRight)
+)ENGINE=InnoDB;
+
+
+#------------------------------------------------------------
+# Table: Member
+#------------------------------------------------------------
+
+CREATE TABLE Member(
+        Id_Member       Int  Auto_increment  NOT NULL ,
+        Name_Member     Varchar (25) NOT NULL ,
+        LastName_Member Varchar (25) NOT NULL ,
+        Password_Member Varchar (50) ,
+        Login_Member    Varchar (50) ,
+        Id_Club         Int ,
+        Id_AccesRight   Int NOT NULL
+	,CONSTRAINT Member_PK PRIMARY KEY (Id_Member)
+
+	,CONSTRAINT Member_Club_FK FOREIGN KEY (Id_Club) REFERENCES Club(Id_Club)
+	,CONSTRAINT Member_AccesRight0_FK FOREIGN KEY (Id_AccesRight) REFERENCES AccesRight(Id_AccesRight)
+)ENGINE=InnoDB;
+
--- README.md
+++
@@ -1,2 +0,0 @@
-# M2LGestionDesAdherent-Clone
-Contenant le projet gestion des adhérent qui a pour contexte la maison des ligues
