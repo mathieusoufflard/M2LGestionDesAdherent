e31f0541e36dc361ec6f6a4f07cd37c456254c11
MathieuAsusRog <mathieusoufflard@gmail.com>
2020-04-06 15:24:25 +0200

    class club and class AccesRight added

---
+++ Developpement/Production/couche métier/Club.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Maison_des_ligues.couche_métier
+{
+    class Club
+    {
+        private int Id { get; set; }
+        private string Name { get; set; }
+
+        public Club(int id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+    }
+}
